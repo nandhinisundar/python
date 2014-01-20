4b96dc9e54bdf7af3f8c9c09cb6e11fa0c75f738
nandhinisundar <nandhinismp@gmail.com>
2014-01-20 20:39:11 +0530

    Create for3.py

---
+++ for3.py
@@ -0,0 +1,12 @@
+for i in range(5):
+ for j in range(i,5):
+  print(" ",end='')
+ for k in range(i):
+  print(" *",end='')
+ print()
+for i in range(5):
+ for j in range(i):
+  print(" ",end='')
+ for k in range(i,5):
+  print(" *",end='')
+ print()
